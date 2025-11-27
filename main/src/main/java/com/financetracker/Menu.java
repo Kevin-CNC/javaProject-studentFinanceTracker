13bf0e8ad4f1300e1c4413d38cb95606d680f20b
Kevin-CNC <kevincncaplescu@gmail.com>
2025-11-27 19:55:58 +0000

    Performed slight fixes related to java compatibility

--- main/src/main/java/com/financetracker/Menu.java
+++ main/src/main/java/com/financetracker/Menu.java
@@ -5,12 +5,31 @@ public class Menu {
     // Constructor that fires the menu loop upon program start.
     public Menu(){
         Scanner inputScan = new Scanner(System.in);
+        boolean inMenu = true;
 
-        while(true){
+        while(inMenu){
             __showMenu__();
 
             int choice = inputScan.nextInt();
-            // TODO: Add functionality of choices + error / logic handling.
+            switch( choice ){ 
+                case 1: 
+                    System.out.println("Adding expense yes");
+                    break;
+                case 2: 
+                    System.out.println("view expenses yes");
+                    break;
+                case 3: 
+                    System.out.println("show total expenses yes");
+                    break;
+                case 4: 
+                    System.out.println("show critical expense yes");
+                    break;
+                default: 
+                    System.out.println("exiting yes");
+                    inMenu = false;
+                    break;
+            }
+
         }
     }
 
@@ -18,17 +37,14 @@ public class Menu {
     /* Modularise menu showing to avoid lengthy code in the main
     system loop */
     public static void __showMenu__(){
-        System.out.println("""
-=-=-= Welcome to the student expense manager! =-=-=
-Please use the following options to interact with
-the program:
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-            1 * Add a new expense
-            2 * View all expenses
-            3 * Show total expense
-            4 * Show critical expenses
-            5 * Exit
-            """);
+        System.out.println("=-=-= Welcome to the student expense manager! =-=-=\n"+
+        "Please use the following options to interact with\n"+
+        "the program:\n"+
+        "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"+
+        "            1 * Add a new expense\n"+
+        "            2 * View all expenses\n" +
+        "            3 * Show total expense\n" +
+        "            4 * Show critical expenses\n" +
+        "            5 * Exit\n");
     }
 }
